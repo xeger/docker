1d79dc3439a73595f0ce817783b76a68574e6852
Roberto Quintanilla <roberto.quintanilla@gmail.com>
2017-04-04 21:46:22 -0500

    Added custom Getopt module to interpret list opts correctly

--- lib/docker.rb
+++ lib/docker.rb
@@ -2,6 +2,7 @@ require_relative 'docker/version'
 require_relative 'docker/error'
 require_relative 'docker/asset'
 require_relative 'docker/container'
+require_relative 'docker/cli'
 require_relative 'docker/session'
 
 module Docker
